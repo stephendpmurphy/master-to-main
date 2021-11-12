2caceb2d09d49fc95fcd03eb1412bae846937096
stephendpmurphy <stephendpmurphy@msn.com>
2021-11-12 13:29:15 -0500

    Add helpful links for renaming branches on Github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Container, Grid, Header, Search} from 'semantic-ui-react';
+import {Container, Header, Search, Message} from 'semantic-ui-react';
 import axios from 'axios';
 import RepoList from './components/RepoList';
 
@@ -9,6 +9,7 @@ const App = () => {
   const [repos, setRepos] = useState([]);
 
   useEffect(() => {
+    // If user is emtpy. Just set the repos list blank and return
     if( user === "" ) {
       setRepos([]);
       return;
@@ -18,50 +19,59 @@ const App = () => {
       try {
         // Retrieve the first page of repos
         const {data} = await axios.get(`https://api.github.com/users/${user}/repos`);
-        // console.log(data);
 
         // If there are more, retreive the next page
 
-
-        const badRepos = [];
+        // Filter through and only grab repos with master as the default branch
+        // and if it's NOT a fork.  (We only want to show repos that the user can do something about)
+        var filteredRepos = [];
         data.forEach((i) => {
           if( (i.default_branch === 'master') && (!i.fork) )
-            badRepos.push(i);
+          filteredRepos.push(i);
         })
 
-        console.log(badRepos);
-        setRepos(badRepos);
-
+        setRepos(filteredRepos);
       }
       catch(err) {
+        // Error occured fetching repos. Clear the list.
         setRepos([]);
       }
     };
 
+    // Debounce our API request. No typing must occur for X time before
+    // we retrieve the repos
     const timeout = setTimeout(() => {
       getRepos();
     }, 500);
 
+    // At the next re-render, clear our timeout thus clearing the debounce.
     return () => {
       clearTimeout(timeout);
     }
-
   }, [user]);
 
   return(
-      <Container>
-        <Grid stretched={true} style={{position:'absolute', top:'50%', left:'50%', width:'75vh', marginTop:'-15%', marginLeft:'-25%'}}>
-          <Grid.Column textAlign='center' verticalAlign='middle' stretched={true}>
-            <Header as='h2' content='Master to Main' subheader="Check if you have any public Github repositories with the default branch set as 'master'"/>
-            <Search
-              size='big'
-              input={{fluid:true}}
-              showNoResults={false}
-              onSearchChange={(e) => setUser(e.target.value)}
-            />
-            { repos.length ? <RepoList repoList={repos}></RepoList> : (user === '' ? null : <Header.Subheader style={{marginTop:'2rem'}}>No user found.</Header.Subheader>)}
-          </Grid.Column>
-        </Grid>
+      <Container style={{height:'100vh', paddingTop:'10vh'}}>
+        <Header textAlign='center' as='h2' content='Master to Main' subheader="Check if you have any public Github repositories with the default branch set as 'master'"/>
+        <Search
+          size='big'
+          input={{fluid:true}}
+          showNoResults={false}
+          onSearchChange={(e) => setUser(e.target.value)}
+          placeholder='Search Github users'
+        />
+        <Message>
+          <Message.Header>Helpful links</Message.Header>
+          <Message.List>
+          <Message.Item>
+            <a href="https://github.com/github/renaming#renaming-existing-branches" target="_blank" rel="noreferrer">Renaming existing branches on Github</a>
+          </Message.Item>
+          <Message.Item>
+            <a href="https://github.com/stephendpmurphy/master-to-main" target="_blank" rel="noreferrer">Contribute to this project on Github</a>
+          </Message.Item>
+          </Message.List>
+        </Message>
+        { repos.length ? <RepoList repoList={repos}></RepoList> : (user === '' ? null : <Header textAlign="center" style={{marginTop:'2rem'}}>No user found.</Header>)}
       </Container>
   );
 }
